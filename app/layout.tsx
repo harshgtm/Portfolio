339593e655f5d9ff48eacdfdc7917c386d423573
Harsh Gautam <harshgautam897@gmail.com>
2026-06-17 00:31:16 +0530

    Fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,9 +91,6 @@ export const metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID;
-  if (!GA_ID) {
-    throw new Error("Missing Google Analytics ID");
-  }
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -117,7 +114,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ModalProvider />
         </ThemeProvider>
       </body>
-      <GoogleAnalytics gaId={GA_ID} />
+      {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
     </html>
   );
 }
